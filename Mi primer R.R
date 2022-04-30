137b87ab6a9e2cf8a717a777d68c5e87213c62fb
angela2020 <angelalaramoreira@gmail.com>
2022-04-29 21:04:05 -0400

    Mi primer R
    Primer uso de R-proyect

---
+++ Mi primer R.R
@@ -0,0 +1,26 @@
+
+
+
+#suma
+4 +4
+
+#multiplicación
+3*5
+
+#división
+4 / 6
+
+#ELevado
+3**2
+
+#Raiz
+4**1/2
+# se guarda en respuesta a la operación
+# 4 + 4
+respuesta <- 4 + 4
+
+print(respuesta)
+
+# almacenando texto
+respuesta <- "que tengan un bonito descando"
+print(respuesta)
