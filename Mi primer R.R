3f0d12ad0c852292772750b7e3d27a015d0ca2ec
glorisss <gdiazm@utem.cl>
2022-07-01 21:03:16 -0400

    Importación de excel
    terminar for
    funciones
    importar excel

--- Mi primer R.R
+++ Mi primer R.R
@@ -66,6 +66,7 @@ var_list <- list("Andres",1,T,"Ana")
 print(var_list[[6]])
 
 len_var_list <-length(var_list)
+print(len_var_list)
 
 var_list <- c(var_list, "hola")
 print(var_list)
@@ -105,27 +106,70 @@ for (i in 1:10) {
 
 
 # ¿Cómo muestro los elementos de una lista con for?
-var_list <- list("Andres",1,T,"Ana",F,"loco","día bonito")
+var_list <- list("Andres",1,T,"Ana","Juan",F,"loco","día bonito")
 
 
 
 #forma Nro 1
 lista_b <- list()
 
-for(i in var_list){
-  print(i)
-  if(1 == "Ana"){
+for(elemento_de_lista in var_list){
+  print("========= nueva iteración ==========")
+  print(elemento_de_lista)
+  if(elemento_de_lista == "Ana"){
+    print("voy a cambiar Ana por JUan")
     lista_b <- c(lista_b,"Juan")
   }else{
-    lista_b <- c(lista_b,i)
+    lista_b <- c(lista_b,elemento_de_lista)
   }
   Sys.sleep(2)
 }
 print(lista_b)
 
+#######################################################
+
+resp_length <- length(var_list)
+for (posición in 1:length(var_list)) {
+  print("=======Nueva Iteración========")
+  print(paste("estamos en la posición: ",posición," - el valor es:", var_list[posición]))
+  Sys.sleep(2)
+}
 
 
 
+############################################################################################
+###########################FUNCIONES########################################################
 
 
+reemplazar <- function(vart_iter,val,reemp = ""){
+  print("Init Funcion reemplazar")
+  respuesta <- list()
+  for (elemento in vart_iter){
+    print("=======NUVA ITERACION=========")
+    print(paste("elemento:",elemento))
+    if(elemento == val){
+      print(paste("Se reemplaza:'",elemento,"'por'", reemp,"'",sep ="" ))
+    respuesta <- c(respuesta, reemp)
+  }else{
+      respuesta <- c(respuesta,elemento)
+  }
+    Sys.sleep(2)
+    }
+  print("Fin Funcion reemplazar")
+  return(respuesta)
+}
 
+var_list <- list("Andres",1,T,"Ana","Juan",F,"loco","día bonito")
+
+var_list <- reemplazar(var_list,"loco","cuerdo")
+
+var_list <- reemplazar(var_list,"Juan")
+
+
+############################################################################################
+############################### EXCEL ######################################################
+
+
+library(readxl)
+Hoja_de_Trabajo_FUNCION_BUSCAR_Aula <- read_excel("Hoja de Trabajo FUNCION BUSCAR Aula.xlsx")
+View(Hoja_de_Trabajo_FUNCION_BUSCAR_Aula)
